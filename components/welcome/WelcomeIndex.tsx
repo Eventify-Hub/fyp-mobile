d5d3203696b2102dd7c7871d0e800a8ca61d0c5e
midhatriz14 <midhatriz14@gmail.com>
2025-05-17 22:48:14 +0500

    Committing notifications work

--- components/welcome/WelcomeIndex.tsx
+++ components/welcome/WelcomeIndex.tsx
@@ -1,42 +1,26 @@
-// import React, { useEffect } from 'react';
-// import { View } from 'react-native';
-// import WelcomeImageDisplay from './WelcomeImageDisplay';
-// import { router } from 'expo-router';
-
-
-
-// const WelcomeIndex: React.FC = () => {
-//   useEffect(() => {
-//     setTimeout(() => {
-//       // router.push("/splashscreen");
-//       router.push("/dashboard");
-//     }, 1000)
-//   }, []);
-//   return (
-//     <View>
-//       <WelcomeImageDisplay imageUri="https://cdn.builder.io/api/v1/image/assets/TEMP/ee8619c3ba7069ac2ac92e880c53f6a08b69c1a800aaf83f5653c512dd5631a5?apiKey=0a92af3bc6e24da3a9ef8b1ae693931a&" />
-//     </View>
-//   );
-// };
-
-// export default WelcomeIndex;
 import { getSecureData } from '@/store';
 import { router } from 'expo-router';
 import React, { useEffect } from 'react';
 import { View } from 'react-native';
 import WelcomeImageDisplay from './WelcomeImageDisplay';
+import { registerForPushNotificationsAsync } from '@/utils/notifications';
+import postPushToken from '@/services/postPushToken';
 
 const WelcomeIndex: React.FC = () => {
   useEffect(() => {
     const redirectUser = async () => {
-      console.log("welcome")
       const userData = await getSecureData('user');
-      console.log("userdata", userData);
       if (!userData || !JSON.parse(userData || "")) {
         router.push('/intro');
       } else {
         try {
           const user = JSON.parse(userData || "");
+          await registerForPushNotificationsAsync().then(token => {
+            console.log("Token", token);
+            if (token) {
+              push(token, user._id)
+            }
+          });
           if (user.role === 'Vendor') {
             router.push('/vendordashboard');
           } else if (user.role === 'Organizer') {
@@ -56,6 +40,19 @@ const WelcomeIndex: React.FC = () => {
       //router.push("/vendoreditprofile");
     }, 1000);
   }, []);
+
+  useEffect(() => {
+    // registerForPushNotificationsAsync().then(token => {
+    //   console.log("Token", token);
+    //   if (token) {
+    //     push(token)
+    //   }
+    // });
+  }, []);
+
+  const push = async (token: string, userId: string) => {
+    await postPushToken(userId, token);
+  }
 
   return (
     <View>
